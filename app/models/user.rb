c282e4fb78b445e4266e5133c7f242edcb470669
pankaj taneja <pankajt@thinknear.com>
2015-02-19 13:41:10 -0800

    Finish log in/log/out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ActiveRecord::Base
+	attr_accessor :remember_token
 	validates(:name, presence: true)
 has_secure_password
+def User.new_token
+	SecureRandom.urlsafe_base64
+end
+def User.digest(string)
+	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+end
+def remember
+	self.remember_token = User.new_token
+	update_attribute(:remember_digest, User.digest(remember_token)) 
+	update_attribute(:remember_digest, )
 end
